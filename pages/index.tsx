de1720ae7dbed73f62576533bc91922119bf48fd
saurabhku463 <saurabhku463@gmail.com>
2022-09-05 19:33:56 +0530

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>Home</title>
+                <meta name="description" content="Macos clone" />
+                <link rel="icon" href="/favicon.ico" />
+                {/* TODO: icon  */}
+            </Head>
+
+            <main> Main Page</main>
+        </div>
+    );
+};
+
+export default Home;
